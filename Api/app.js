27ff1bafb632b0eff3ec7b28ed2fee06f3ed66d3
jonatanb28 <jonatan.balbuena@gmail.com>
2022-04-26 23:51:56 -0300

    m

--- Api/app.js
+++ Api/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/wines', productRoutes)
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 
 try {
     // await db.authenticate()
