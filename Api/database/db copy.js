f9570a572c7d7a9543c3adf222ff1cb0bbf022db
jonatanb28 <jonatan.balbuena@gmail.com>
2022-04-26 23:34:57 -0300

    m

--- Api/database/db copy.js
+++
@@ -1,9 +0,0 @@
-import {Sequelize} from 'sequelize';
-
-const db = new Sequelize('heroku_adaf35e517ce1e5', 'bca6d519d5c372', '65bc54b4', {
-    host: 'us-cdbr-east-05.cleardb.net',
-    dialect: 'mysql'
-})
-
-
-export default db;
