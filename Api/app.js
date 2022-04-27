36756df80ee12458d4fd6f29a2c0aa4e0548bf6e
jonatanb28 <jonatan.balbuena@gmail.com>
2022-04-26 23:59:21 -0300

    m

--- Api/app.js
+++ Api/app.js
@@ -1,23 +1,23 @@
 import express from "express";
 import cors from 'cors';
-// import db from "./database/db.js";
+import db from "./database/db.js";
 import productRoutes from './routes/routes.js';
 
 const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors() );
+app.use(express.json() );
 app.use('/wines', productRoutes)
 
 const PORT = process.env.PORT || 8000;
 
 try {
-    // await db.authenticate()
+    await db.authenticate()
     console.log('conexión exitosa')
 } catch (error) {
     console.log(`error: ${error}`)
 }
 
 app.listen(PORT, ()=>{
-    console.log('server running in http://localhost:8000/')
+    console.log(`server running in ${PORT}`)
 })
