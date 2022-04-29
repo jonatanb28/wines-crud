4e231e50533835497f1127167fa148991627d9bf
jonatanb28 <jonatan.balbuena@gmail.com>
2022-04-29 15:55:34 -0300

    i

--- Api/app.js
+++ Api/app.js
@@ -6,7 +6,7 @@ import productRoutes from './routes/routes.js';
 const app = express()
 
 app.use(cors() );
-app.use(express.json() );
+app.use(express.json());
 app.use('/wines', productRoutes)
 
 const PORT = process.env.PORT || 8000;
--- client/src/Components/CreateProduct.jsx
+++ client/src/Components/CreateProduct.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import {useDispatch, useSelector} from 'react-redux'
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
 import { createProduct, getAllBrands, getAllStrains, getAllTypes } from "../Redux/Actions";
 import './inputs.css'
